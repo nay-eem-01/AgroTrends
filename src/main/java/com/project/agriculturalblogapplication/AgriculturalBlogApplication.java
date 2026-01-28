00a6e2bafc51e3a8034de60ca99c1cb4125b2b4e
nay-eem-01 <ahmedmdnayeem05@gmail.com>
2026-01-28 23:26:13 +0600

    fix: add auditing annotation

--- src/main/java/com/project/agriculturalblogapplication/AgriculturalBlogApplication.java
+++ src/main/java/com/project/agriculturalblogapplication/AgriculturalBlogApplication.java
@@ -2,8 +2,10 @@ package com.project.agriculturalblogapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AgriculturalBlogApplication {
 
 	public static void main(String[] args) {
